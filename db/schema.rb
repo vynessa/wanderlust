eada7b2b8fef3cd8ff0b1b5f5dbfe9582cb34031
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-02-11 21:34:44 +0100

    Modify files

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20180211102321) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -48,8 +50,6 @@ ActiveRecord::Schema.define(version: 20180211102321) do
     t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
