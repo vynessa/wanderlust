aa1ef0fcc4be6ad4a9b5b8cf011d4103e0c42cff
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-02-15 10:37:43 +0100

    Create budget items model and migrations data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213185724) do
+ActiveRecord::Schema.define(version: 20180214101249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20180213185724) do
   create_table "activities", force: :cascade do |t|
     t.string "title"
     t.boolean "status"
+    t.integer "trip_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "budget_items", force: :cascade do |t|
+    t.string "item"
+    t.text "description"
+    t.integer "cost"
     t.integer "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -35,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180213185724) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
