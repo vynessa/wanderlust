3772437eb04b4ce804ed6e333f95ea759cca9582
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-02-15 11:15:13 +0100

    Remove password column from user table

---
+++ db/migrate/20180215101304_remove_password_col_on_users.rb
@@ -0,0 +1,5 @@
+class RemovePasswordColOnUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214101249) do
+ActiveRecord::Schema.define(version: 20180215101304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20180214101249) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
