83108608a485fb5ae54e27f1c4a59c99e5961439
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-02-17 18:42:22 +0100

    Make changes to fix slips from pull request review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def fill_background_color
-    fill_background = if user_signed_in?
-                        color = "#ffffff"
-                      else
-                        color = "#042852"
-                      end
+    color = if user_signed_in? 
+              "#ffffff" 
+            else 
+              "#042852" 
+            end
   end
 end
