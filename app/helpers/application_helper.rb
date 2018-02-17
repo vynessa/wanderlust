da7df0cc0aeca48c95f58962bd474f908e8b510e
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-02-17 17:10:04 +0100

    feat(landing-page): design landing page
    - create mixins to be reused within the application
    - style the landing page
    
    [Delivers #155251745]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fill_background_color
+    fill_background = if user_signed_in?
+                        color = "#ffffff"
+                      else
+                        color = "#042852"
+                      end
+  end
 end
